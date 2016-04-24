b84154c79a420be08f85912ffcdd838975d9b4ed
savidger <rob@root-2.com>
2016-04-24 14:49:56 -0700

    Add Weather

--- Dronr-Ionic/www/js/controllers.js
+++ Dronr-Ionic/www/js/controllers.js
@@ -14,7 +14,7 @@ angular.module('starter.controllers', [])
 
   $scope.$watch(function () {return DronrUpdate.value;}, function(newDronrData, oldDronrData)
   {
-	  if(newDronrData){    
+	  if(newDronrData){
 		if (newDronrData.location){
 			console.log(newDronrData);
 			updatePosition(newDronrData.location.lat, newDronrData.location.lng);
@@ -23,19 +23,19 @@ angular.module('starter.controllers', [])
 			for(var key in newDronrData.no_fly_zones){
 				var noFlyZone = newDronrData.no_fly_zones [key];
 				var drawNoFlyZone = true;
-				
+
 				for(var currentNoFlyZoneKey in noFlyZones){
 					var currentNoFlyZone = noFlyZones[currentNoFlyZoneKey];
 					if(currentNoFlyZone.name == noFlyZone.name){
 						drawNoFlyZone = false;
 					}
 				}
-				
+
 				if(drawNoFlyZone == true){
-					
+
 				if(noFlyZone.type == "polygon"){
 					var Coords = noFlyZone.coords;
-					
+
 					var restrictedAirspace = new google.maps.Polygon({
 						paths: Coords,
 						strokeColor: '#FF0000',
@@ -45,11 +45,11 @@ angular.module('starter.controllers', [])
 						fillOpacity: 0.35
 						});
 					restrictedAirspace.setMap(map);
-					
+
 					noFlyZones.push(noFlyZone);
 				}
 				else{
-					
+
 					var restrictedAirspaceAirport = new google.maps.Circle({
 						strokeColor: '#FF0000',
 						strokeOpacity: 0.8,
@@ -60,14 +60,14 @@ angular.module('starter.controllers', [])
 						center: noFlyZone.coords[0],
 						radius: 8046.72
 					});
-					
+
 					noFlyZones.push(noFlyZone);
 				}
 				}
 			}
 		}
 	  }
-	
+
   });
 
   var updatePosition = function (lat, lng){
@@ -86,7 +86,85 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('WeatherCtrl', function($scope) {
+.controller('WeatherCtrl', function($scope, DronrUpdate) {
+  var weather = {
+    "time": "24 Apr 11:150 am PDT",
+    "temp": 29.2,
+    "wind_speed": 1.5,
+    "wind_direction": 1,
+    "wind_gust": 4.5,
+    "precip": 0.5,
+    "visibility": 10.1,
+    "hazards":
+    [
+      {
+        "desc": "This is a hazard.",
+        "url": "http://www.weather.gov/hazard_url"
+      }
+    ]
+  };
+
+  $scope.$watch(function () {return DronrUpdate.value;}, function(newDronrData, oldDronrData) {
+    if (newDronrData && newDronrData.weather) {
+      weather = newDronrData.weather;
+    }
+  });
+
+  $scope.weather = weather;
+})
+
+.filter('degreesToDirection', function() {
+  return function (degrees) {
+    if (degrees >= 348.75 || (degrees >= 0 && degrees < 11.25)) {
+      return 'N';
+    }
+    else if (degrees >= 11.25 && degrees < 33.75) {
+      return 'NNE'
+    }
+    else if (degrees >= 33.75 && degrees < 56.25) {
+      return 'NE'
+    }
+    else if (degrees >= 56.25 && degrees < 78.75) {
+      return 'ENE'
+    }
+    else if (degrees >= 78.75 && degrees < 101.25) {
+      return 'E'
+    }
+    else if (degrees >= 101.25 && degrees < 123.75) {
+      return 'ESE'
+    }
+    else if (degrees >= 123.75 && degrees < 146.25) {
+      return 'SE'
+    }
+    else if (degrees >= 146.25 && degrees < 168.75) {
+      return 'SSE'
+    }
+    else if (degrees >= 168.75 && degrees < 191.25) {
+      return 'S'
+    }
+    else if (degrees >= 191.25 && degrees < 213.75) {
+      return 'SSW'
+    }
+    else if (degrees >= 213.75 && degrees < 236.25) {
+      return 'SW'
+    }
+    else if (degrees >= 236.25 && degrees < 258.75) {
+      return 'WSW'
+    }
+    else if (degrees >= 258.75 && degrees < 281.25) {
+      return 'W'
+    }
+    else if (degrees >= 281.25 && degrees < 303.75) {
+      return 'WNW'
+    }
+    else if (degrees >= 303.75 && degrees < 326.25) {
+      return 'NW'
+    }
+    else if (degrees >= 326.25 && degrees < 348.75) {
+      return 'NNW'
+    }
+    else return 'unknown';
+  }
 })
 
 .controller('FlightPathCtrl', function($scope) {
