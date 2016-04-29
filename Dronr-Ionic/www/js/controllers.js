df35541c3ef5347069c71958a38a53a1e9faf792
savidger <rob@root-2.com>
2016-04-29 08:13:06 -0700

    Made weather more pretty.

--- Dronr-Ionic/www/js/controllers.js
+++ Dronr-Ionic/www/js/controllers.js
@@ -5,88 +5,88 @@ angular.module('starter.controllers', [])
 
   var mapOptions = {
     center: myLatlng,
-    zoom: 12,
+    zoom: 11,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
   var noFlyZones = [];
-  
+
   var dronrMarker;
 
   $scope.$watch(function () {return DronrUpdate.value;}, function(newDronrData, oldDronrData)
   {
-	  if(newDronrData){
-		if (newDronrData.location){
-			console.log(newDronrData);
-			updatePosition(newDronrData.location.lat, newDronrData.location.lng);
-		}
-		if(newDronrData.no_fly_zones){
-			for(var key in newDronrData.no_fly_zones){
-				var noFlyZone = newDronrData.no_fly_zones [key];
-				var drawNoFlyZone = true;
+    $scope.hazards = [];
+	  if(newDronrData) {
+      if (newDronrData.location) {
+        console.log(newDronrData);
+        if(newDronrData.weather) {
+          $scope.hazards = newDronrData.weather.hazards;
+        }
+        updatePosition(newDronrData.location.lat, newDronrData.location.lng);
+      }
 
-				for(var currentNoFlyZoneKey in noFlyZones){
-					var currentNoFlyZone = noFlyZones[currentNoFlyZoneKey];
-					if(currentNoFlyZone.name == noFlyZone.name){
-						drawNoFlyZone = false;
-					}
-				}
+      if (newDronrData.no_fly_zones) {
+        for (var key in newDronrData.no_fly_zones) {
+          var noFlyZone = newDronrData.no_fly_zones [key];
+          var drawNoFlyZone = true;
 
-				if(drawNoFlyZone == true){
+          for (var currentNoFlyZoneKey in noFlyZones) {
+            var currentNoFlyZone = noFlyZones[currentNoFlyZoneKey];
+            if (currentNoFlyZone.name == noFlyZone.name) {
+              drawNoFlyZone = false;
+            }
+          }
 
-				if(noFlyZone.type == "polygon"){
-					var Coords = noFlyZone.coords;
+          if (drawNoFlyZone == true) {
+            if (noFlyZone.type == "polygon") {
+              var Coords = noFlyZone.coords;
 
-					var restrictedAirspace = new google.maps.Polygon({
-						paths: Coords,
-						strokeColor: '#FF0000',
-						strokeOpacity: 0.8,
-						strokeWeight: 2,
-						fillColor: '#FF0000',
-						fillOpacity: 0.35
-						});
-					restrictedAirspace.setMap(map);
+              var restrictedAirspace = new google.maps.Polygon({
+                paths: Coords,
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35
+              });
+              restrictedAirspace.setMap(map);
 
-					noFlyZones.push(noFlyZone);
-				}
-				else{
+              noFlyZones.push(noFlyZone);
+            } else {
+              var restrictedAirspaceAirport = new google.maps.Circle({
+                strokeColor: '#FF0000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: '#FF0000',
+                fillOpacity: 0.35,
+                map: map,
+                center: noFlyZone.coords[0],
+                radius: 8046.72
+              });
 
-					var restrictedAirspaceAirport = new google.maps.Circle({
-						strokeColor: '#FF0000',
-						strokeOpacity: 0.8,
-						strokeWeight: 2,
-						fillColor: '#FF0000',
-						fillOpacity: 0.35,
-						map: map,
-						center: noFlyZone.coords[0],
-						radius: 8046.72
-					});
-
-					noFlyZones.push(noFlyZone);
-				}
-				}
-			}
-		}
-	  }
-
+              noFlyZones.push(noFlyZone);
+            }
+          }
+        }
+      }
+    }
   });
 
-  var updatePosition = function (lat, lng){
-	var latLong = new google.maps.LatLng(lat, lng);
+  var updatePosition = function (lat, lng) {
+    var latLong = new google.maps.LatLng(lat, lng);
     map.setCenter(latLong);
-	if(!dronrMarker){
-		  dronrMarker = new google.maps.Marker({
-			position: latLong,
-			title: 'Dronr',
-			map: map
-			});
-	  }
-	  else{
-		  dronrMarker.setPosition(latLong);
-	  }
+    if (!dronrMarker) {
+      dronrMarker = new google.maps.Marker({
+        position: latLong,
+        title: 'Dronr',
+        map: map
+      });
+    } else {
+      dronrMarker.setPosition(latLong);
+    }
   };
-  
+
   $scope.map = map;
   updatePosition ({latitude: 37.3000, longitude: -120.4833});
 })
@@ -118,7 +118,7 @@ angular.module('starter.controllers', [])
   };
 
   $scope.$watch(function () {return DronrUpdate.value;}, function(newDronrData, oldDronrData) {
-    if (newDronrData && newDronrData.weather) {
+    if(newDronrData && newDronrData.weather){
       $scope.weather = newDronrData.weather;
     }
   });
@@ -181,7 +181,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('FlightPathCtrl', function($scope) {
-  
+
 })
 
 .controller('SettingsCtrl', function($scope, DronrSettings) {
